5bb6fd36aacb6b289de795fe3be786f336d36ffd
Goliaeth <75118259+Goliaeth@users.noreply.github.com>
2022-09-12 10:39:16 +0500

    add rollDice function

--- app/src/main/java/com/nick/diceapplication/MainActivity.kt
+++ app/src/main/java/com/nick/diceapplication/MainActivity.kt
@@ -2,10 +2,31 @@ package com.nick.diceapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.btn_roll_dice)
+
+        rollButton.setOnClickListener {
+
+            rollDice()
+
+        }
+
     }
+
+    private fun rollDice() {
+
+        val randomNumber = (1..6).random()
+        val randomNumberTxt = findViewById<TextView>(R.id.txt_view_random_number)
+        randomNumberTxt.text = randomNumber.toString()
+
+    }
+
 }
